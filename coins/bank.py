216cd4b4b1eb2e833873cc9bb069fc1655cd35cb
CharliW90 <acey.williams@googlemail.com>
2024-07-10 19:59:18 +0100

    implemented check() function that checks that the bank hasn't run out of ones or fives, and if it has attempts to exchange with all players, providing them with higher denominations in exchange for their lower denominations

--- coins/bank.py
+++ coins/bank.py
@@ -1,14 +1,17 @@
-from collections import Counter
-from coins.coinage import *
-from coins.transactions import *
+from reference import reference
+from coins.coinage import CoinPiles
+from coins.transactions import giving, receiving
 
 class Bank:
   def __init__(self):
     self.name = "The Bank"
-    self.coins = CoinPiles(42, 24, 12)
+    self.colour = 'cyan'
+    self.colorize = reference['ansiColours'][self.colour]
+    self.reset = reference["ansiColours"]["reset"]
+    self.coins = CoinPiles(24, 24, 12)
     self.total = 282
     
-  def __repr__(self):
+  def __str__(self):
     self.updateTotal()
     output = (
     f"{self.name} contains {self.total} in coinage:\n"
@@ -17,6 +20,9 @@ class Bank:
     f"{len(self.coins.golds)} Gold Tens, valuing {sum([coin.value for coin in self.coins.golds])}"
     )
     return output
+  
+  def declareAction(self, str):
+    print(f"{self.colorize}{str}{self.reset}")
 
   def updateTotal(self):
     self.total = self.coins.total()
@@ -26,11 +32,24 @@ class Bank:
 
   def takePayment(self, coins, totalToPay):
     payment = receiving(self, coins) # put payment into bank's coin pile
-    print(f"{self.name} received {payment} towards {totalToPay} requested.")
     self.updateTotal()
     return giving(self, payment-totalToPay) # give change, if any
 
+  def check(self, game):
+    ones = len(self.coins.coppers)
+    fives = len(self.coins.silvers)
+    if not ones or not fives:
+      if not ones:
+        self.declareAction(f"{self.name} has {ones} copper One coins remaining - exchanging up with players...")
+      elif not fives:
+        self.declareAction(f"{self.name} has {fives} silver Five coins remaining - exchanging up with players...")
+      for player in game.players:
+        print(len(player.coins.coppers), len(player.coins.silvers), len(player.coins.golds))
+        coins = player.giveAll()
+        player.receive(self.exchange(coins))
+        print(len(player.coins.coppers), len(player.coins.silvers), len(player.coins.golds))
+
   def exchange(self, coins):
     intake = receiving(self, coins) # put all coins into the bank's coin pile
-    return givePlayer(self, intake) # give back the same value in coins, starting with highest denomination
+    return self.givePlayer(intake) # give back the same value in coins, starting with highest denomination
 
