cb65ed89ef33cd0416e0f8948d78ffd76981e778
CharliW90 <acey.williams@googlemail.com>
2024-07-10 19:57:45 +0100

    tweak: adjusted 'giving' variable name to not match function name, and to be past tense same as for receiving()/received

--- coins/transactions.py
+++ coins/transactions.py
@@ -1,37 +1,37 @@
 def giving(self, total):
-  giving = []
+  given = []
   if total == 0:
-    return giving
+    return given
   else:
     remaining = total
     while remaining > 0:
       if remaining >= 10 and len(self.coins.golds) > 0:
         remaining -= 10
-        giving.append(self.coins.golds.pop())
+        given.append(self.coins.golds.pop())
       elif remaining >= 5 and len(self.coins.silvers) > 0:
         remaining -= 5
-        giving.append(self.coins.silvers.pop())
+        given.append(self.coins.silvers.pop())
       elif remaining >= 1 and len(self.coins.coppers) > 0:
         remaining -= 1
-        giving.append(self.coins.coppers.pop())
+        given.append(self.coins.coppers.pop())
       elif len(self.coins.silvers) > 0: # make overpayment
         remaining -= 5
-        giving.append(self.coins.silvers.pop())
+        given.append(self.coins.silvers.pop())
       elif len(self.coins.golds) > 0:
         remaining -= 5
-        giving.append(self.coins.golds.pop())
+        given.append(self.coins.golds.pop())
       else:
         remaining = 0
         print(f"{self.name} has no more coins to give.")
 
-    totalGiving = sum([coin.value for coin in giving])
+    totalGiving = sum([coin.value for coin in given])
     if remaining > 0:
-      print(f"{self.name} is giving {totalGiving}. {total} requested - payment was short by {remaining}!")
+      self.declareAction(f"{self.name} is giving {totalGiving}. {total} requested (payment was short by {remaining}) ==>")
     elif remaining < 0:
-      print(f"{self.name} is giving {totalGiving}. {total} requested - requires {abs(remaining)} change!")
+      self.declareAction(f"{self.name} is giving {totalGiving}. {total} requested (requires {abs(remaining)} change) ==>")
     else:
-      print(f"{self.name} is giving {totalGiving}.")
-    return giving
+      self.declareAction(f"{self.name} is giving {totalGiving} ==>")
+    return given
 
 def receiving(self, coins):
   received = 0
@@ -44,7 +44,7 @@ def receiving(self, coins):
     elif coin.colour == "Gold":
       self.coins.golds.append(coin)
   if received > 0:
-    print(f"{self.name} received {sum([coin.value for coin in coins])} cash")
+    self.declareAction(f"==> {self.name} received {sum([coin.value for coin in coins])} cash")
   return received
 
 def calcPayment(coins, total):
