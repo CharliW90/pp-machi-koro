a4b26f98eb21c3b31abe359701d1d0c9739cc207
CharliW90 <acey.williams@googlemail.com>
2024-07-11 17:29:13 +0100

    add validation for name and colour, and prevent changes to these

--- player.py
+++ player.py
@@ -6,18 +6,55 @@ from coins.coinage import CoinPiles
 from cards.stacks import Hand, Abilities
 
 playerColours = ['red','green','blue','purple']
+playerNames = []
+
+def reset():
+  print("Resetting playerNames...")
+  playerNames.clear()
+  return("playerNames reset!")
 
 class Player:
   def __init__(self, name, i):
     self.name = name
-    self.colour = playerColours[i]
-    self.colorize = reference['ansiColours'][self.colour]
+    self.colour = i
+    self.colorize = self.colour
     self.reset = reference["ansiColours"]["reset"]
     self.current = False
     self.buildActionTaken = False
     self.coins = CoinPiles(0, 0, 0)
     self.cards = Hand()
     self.abilities = Abilities()
+    self._initialised = False
+
+  @property
+  def name(self):
+    return self.__name
+  @name.setter
+  def name(self, string):
+    if self._initialised: raise Exception("Sorry, but you can't edit players")
+    if not isinstance(string, (str, int)): raise ValueError(f"Provided names must be plain strings - {string} is a {type(string)}.")
+    if len(str(string)) < 1: raise ValueError("Name cannot be blank!")
+    if str(string) in playerNames: raise Exception(f"Cannot have duplicate player names - {str(string)} has already been used.")
+    playerNames.append(str(string))
+    self.__name = str(string)
+
+  @property
+  def colour(self):
+    return self.__colour
+  @colour.setter
+  def colour(self, i):
+    if self._initialised: raise Exception("Sorry, but you can't edit players")
+    if i > 3: raise ValueError("Somehow you're trying to generate more players than are allowed in a game - this class should not be called independently of the Game class.")
+    self.__colour = playerColours[i]
+  
+  @property
+  def colorize(self):
+    return self.__colorize
+  @colorize.setter
+  def colorize(self, colour):
+    if self._initialised: raise Exception("Sorry, but you can't edit players")
+    if not colour in playerColours: raise ValueError("Somehow the colour for colorize is not one of the valid player colours")
+    self.__colorize = reference["ansiColours"][colour]
   
   def __str__(self):
     cash = self.coins.total()
