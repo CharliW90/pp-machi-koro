117506d24ff3a5e51877a7aa9e1b34a3a0a00c73
CharliW90 <acey.williams@googlemail.com>
2024-07-23 15:44:42 +0100

    implement type hints and refactor code

--- player.py
+++ player.py
@@ -1,20 +1,24 @@
-import time
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+  from game import Game
+  from coins import Bank
+
 from tabulate import tabulate
 from reference import reference
-from coins.transactions import *
-from coins.coinage import CoinPiles
-from cards.stacks import Hand, Abilities
+from coins import CoinPiles, Coin, giving, receiving, calcPayment
+from cards import Hand, Abilities, Blues, Greens, Reds, Purples, Landmarks
 
 playerColours = ['red','green','blue','purple']
 playerNames = []
 
 def reset():
-  print("Resetting playerNames...")
   playerNames.clear()
   return("playerNames reset!")
 
 class Player:
   def __init__(self, name, i):
+    self._initialised = False
     self.name = name
     self.colour = i
     self.colorize = self.colour
@@ -24,7 +28,6 @@ class Player:
     self.coins = CoinPiles(0, 0, 0)
     self.cards = Hand()
     self.abilities = Abilities()
-    self._initialised = False
 
   @property
   def name(self):
@@ -34,7 +37,7 @@ class Player:
     if self._initialised: raise Exception("Sorry, but you can't edit players")
     if not isinstance(string, (str, int)): raise ValueError(f"Provided names must be plain strings - {string} is a {type(string)}.")
     if len(str(string)) < 1: raise ValueError("Name cannot be blank!")
-    if str(string) in playerNames: raise Exception(f"Cannot have duplicate player names - {str(string)} has already been used.")
+    if str(string) in playerNames: raise Exception(f"Cannot have duplicate player names - {str(string)} has already been used.\n{playerNames}")
     playerNames.append(str(string))
     self.__name = str(string)
 
@@ -56,7 +59,7 @@ class Player:
     if not colour in playerColours: raise ValueError("Somehow the colour for colorize is not one of the valid player colours")
     self.__colorize = reference["ansiColours"][colour]
   
-  def __str__(self):
+  def __str__(self) -> str:
     cash = self.coins.total()
     landmarks = 0
     for card in self.cards.landmarks:
@@ -64,66 +67,71 @@ class Player:
         landmarks += 1
     return f"{self.name} has {cash} cash, and has built {landmarks} landmarks"
   
-  def declareAction(self, str):
-    time.sleep(0.2)
-    print(f"{self.colorize}{str}{self.reset}")
+  def declareAction(self, action: str) -> None:
+    print(f"{self.colorize}{action}{self.reset}")
 
-  def beginTurn(self):
+  def beginTurn(self) -> None:
     self.current = True
     self.buildActionTaken = False
     self.declareAction(f"It is {self.name}'s turn!")
 
-  def endTurn(self):
+  def endTurn(self) -> None:
     self.current = False
 
-  def viewHand(self):
+  def viewHand(self) -> None:
     hand = self.cards.contents()
     print(tabulate(hand, ["Name", "Action", "Owned"]))
 
-  def getBalance(self):
+  def getBalance(self) -> int:
     return self.coins.total()
   
-  def activate(self, game, colour, diceRoll):
+  def activate(self, game: Game, colour: str, diceRoll) -> None:
     stack = getattr(self.cards, colour)
     for card in stack:
-      card.trigger(self, diceRoll)
+      card.trigger(game, self, diceRoll)
 
-  def receive(self, coins):
+  def receive(self, coins: list[Coin]) -> int:
     return receiving(self, coins)
   
-  def give(self, total):
+  def give(self, total: int) -> list[Coin]:
     return giving(self, total)
   
-  def giveAll(self):
+  def giveAll(self) -> list[Coin]:
     return giving(self, self.getBalance())
   
-  def build(self, card, bank):
+  def build(self, card: Blues | Greens | Reds | Purples | Landmarks, bank: Bank) -> bool:
     if self.buildActionTaken:
       print(f"You can only take one build action per turn!")
+      return self.buildActionTaken
     else:
       cash = self.coins.total()
-      if(card.type == "Major Establishment"):
+      if(card.cardType == "Major Establishment"):
         stack = getattr(self.cards, 'purple')
         for majorEstablishment in stack:
           if majorEstablishment.title == card.title:
             self.declareAction(f"You already have a {card.title} and can only purchase one {card.title} per game.")
-      elif cash >= card.cost:
-        payment = calcPayment(self.coins, card.cost)
-        self.receive(bank.takePayment(self.give(payment), card.cost))
-        if card.type == "Landmark":
-          stack = getattr(self.cards, 'landmarks')
-          ability = card.build()
-          self.abilities.update(ability)
-        else:
-          self.cards.add(card)
-        self.buildActionTaken = True
-        self.declareAction(f"{self.name} has purchased {card.title} for {card.cost} cash\n{self}")
-      else:
+            return self.buildActionTaken
+
+      if cash < card.cost:
         print(f"{self.name} cannot afford {card.title}")
+        return self.buildActionTaken
 
-    return self.buildActionTaken
+      payment = calcPayment(self.coins, card.cost)
+      self.receive(bank.takePayment(self.give(payment), card.cost))
+      if isinstance(card, Landmarks):
+        stack = getattr(self.cards, 'landmarks')
+        ability = card.build(self)
+        self.abilities.update(ability)
+      else:
+        self.cards.add(card)
 
-  def hasWon(self):
+      self.buildActionTaken = True
+      self.declareAction(f"{self.name} has purchased {card.title} for {card.cost} cash\n{self}")
+      return self.buildActionTaken
+
+  def hasWon(self) -> bool:
+    """Checks to see if the player has built all 4 of their landmark cards
+    returns False if any are not built, otherwise True"""
     for card in self.cards.landmarks:
       if card.built == False:
         return False
