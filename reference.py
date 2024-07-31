c655c1c4f7423b4c47340cbb473598884101bfc4
CharliW90 <acey.williams@googlemail.com>
2024-07-31 15:15:34 +0100

    refactor to list comprehensions, and amend names to snake case

--- reference.py
+++ reference.py
@@ -1,53 +1,53 @@
 from inquirer.themes import Default, term
 
-rgbColours = {
+rgb_colours = {
   'red': [208,0,0],
   'green': [0,208,0],
-  'blue': [0,0,255],
-  'purple': [208,0,255],
-  'orange': [255,104,0],
-  'yellow': [208, 208, 0],
-  'cyan': [52, 255, 208]
+  'blue': [0,0,208]
 }
 
-ansiColours = {
-  'red': f"\033[38;2;{';'.join(str(x) for x in rgbColours['red'])};74m",
-  'green': f"\033[38;2;{';'.join(str(x) for x in rgbColours['green'])};74m",
-  'blue': f"\033[38;2;{';'.join(str(x) for x in rgbColours['blue'])};74m",
-  'purple': f"\033[38;2;{';'.join(str(x) for x in rgbColours['purple'])};74m",
-  'orange': f"\033[38;2;{';'.join(str(x) for x in rgbColours['orange'])};74m",
-  'yellow': f"\033[38;2;{';'.join(str(x) for x in rgbColours['yellow'])};74m",
-  'cyan': f"\033[38;2;{';'.join(str(x) for x in rgbColours['cyan'])};74m",
-  'reset': f"\033[39m"
-}
+rgb_colours['cyan'] = [b+g for b, g in zip(rgb_colours['blue'], rgb_colours['green'])]
+rgb_colours['yellow'] = [r+g for r, g in zip(rgb_colours['red'], rgb_colours['green'])]
+rgb_colours['magenta'] = [r+b for r, b in zip(rgb_colours['red'], rgb_colours['blue'])]
 
-hexColours = {
-  'red': '#%02x%02x%02x' % (*rgbColours['red'],),
-  'green': '#%02x%02x%02x' % (*rgbColours['green'],),
-  'blue': '#%02x%02x%02x' % (*rgbColours['blue'],),
-  'purple': '#%02x%02x%02x' % (*rgbColours['purple'],),
-  'orange': '#%02x%02x%02x' % (*rgbColours['orange'],),
-  'yellow': '#%02x%02x%02x' % (*rgbColours['yellow'],),
-  'cyan': '#%02x%02x%02x' % (*rgbColours['cyan'],)
-}
+rgb_colours['crashmat_blue'] = [b+int(g/2) for b, g in zip(rgb_colours['blue'], rgb_colours['green'])]
+rgb_colours['orange'] = [r+int(g/2) for r, g in zip(rgb_colours['red'], rgb_colours['green'])]
+rgb_colours['pink'] = [r+int(b/2) for r, b in zip(rgb_colours['red'], rgb_colours['blue'])]
+
+rgb_colours['teal'] = [g+int(b/2) for g, b in zip(rgb_colours['green'], rgb_colours['blue'])]
+rgb_colours['lime'] = [g+int(r/2) for g, r in zip(rgb_colours['green'], rgb_colours['red'])]
+rgb_colours['purple'] = [b+int(r/2) for b, r in zip(rgb_colours['blue'], rgb_colours['red'])]
+
+rgb_colours['crashmat_blue_faded'] = [b+int(g/2.5)+int(r/3) for b, g, r in zip(rgb_colours['blue'], rgb_colours['green'], rgb_colours['red'])]
+rgb_colours['orange_faded'] = [r+int(g/2.5)+int(b/3) for r, g, b in zip(rgb_colours['red'], rgb_colours['green'], rgb_colours['blue'])]
+rgb_colours['pink_faded'] = [r+int(b/2.5)+int(g/3) for r, b, g in zip(rgb_colours['red'], rgb_colours['blue'], rgb_colours['green'])]
+
+rgb_colours['teal_faded'] = [int(g/2)+int(b/2) for g, b in zip(rgb_colours['green'], rgb_colours['blue'])]
+rgb_colours['lime_faded'] = [int(g/2)+int(r/2) for g, r in zip(rgb_colours['green'], rgb_colours['red'])]
+rgb_colours['purple_faded'] = [int(b/2)+int(r/2) for b, r in zip(rgb_colours['blue'], rgb_colours['red'])]
+
+ansi_colours = {colour: f"\033[38;2;{';'.join(str(x) for x in rgb_colours[colour])};74m" for colour in rgb_colours}
+ansi_colours['reset'] = f"\033[39m"
+
+hex_colours = {colour: '#%02x%02x%02x' % (*rgb_colours[colour],) for colour in rgb_colours}
 
 shortcuts = {
-  'notificationStart': f"{ansiColours['yellow']}==> ",
-  'notificationEnd': f"{ansiColours['reset']}\n",
+  'notification_start': f"{ansi_colours['yellow']}==> ",
+  'notification_end': f"{ansi_colours['reset']}\n",
   'unaffordable': "\x1b[9;2m",
   'affordable': "\x1b[3;32m",
   'reset': "\x1b[0m",
-  'blinkStart': "\033[5m",
-  'blinkEnd': "\033[0m"
+  'blink_start': "\033[5m",
+  'blink_end': "\033[0m"
 }
 
-reference = {"rgbColours": rgbColours, "ansiColours": ansiColours, "hexColours": hexColours}
+reference = {"rgb_colours": rgb_colours, "ansi_colours": ansi_colours, "hex_colours": hex_colours, "shortcuts": shortcuts}
 
 class MyTheme(Default):
   def __init__(self, player):
     super().__init__()
-    self.Question.mark_color = term.color_rgb(*rgbColours[player.colour]) # type: ignore
-    self.Question.brackets_color = term.color_rgb(*rgbColours[player.colour]) # type: ignore
-    self.Question.default_color = term.on_color_rgb(*rgbColours[player.colour]) # type: ignore
-    self.List.selection_color = term.on_color_rgb(*rgbColours[player.colour]) # type: ignore
+    self.Question.mark_color = term.color_rgb(*rgb_colours[player.colour]) # type: ignore
+    self.Question.brackets_color = term.color_rgb(*rgb_colours[player.colour]) # type: ignore
+    self.Question.default_color = term.on_color_rgb(*rgb_colours[player.colour]) # type: ignore
+    self.List.selection_color = term.on_color_rgb(*rgb_colours[player.colour]) # type: ignore
     self.List.selection_cursor = f"{player.name} =>" # type: ignore
