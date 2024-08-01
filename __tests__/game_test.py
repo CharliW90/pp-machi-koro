056790ff283c1d668197655c3fe6f16f218afa58
CharliW90 <acey.williams@googlemail.com>
2024-08-01 13:23:19 +0100

    abstract away repetitive testing

--- __tests__/game_test.py
+++ __tests__/game_test.py
@@ -1,5 +1,6 @@
 import pytest
 from game import Game
+from player import reset as reset_player_names
 
 onePlayer = ['A']
 twoPlayer = [*onePlayer, 'B']
@@ -10,8 +11,29 @@ bankTemplate = [42, 24, 12]
 cashTemplate = [42, 120, 120]
 coloursTemplate = ['red','green','blue','purple', 'orange', 'yellow', 'cyan']
 
+def assess_game_class(game):
+  assert hasattr(game, 'name')
+  assert game.name == "Machi Koro"
+  assert hasattr(game, 'player_count')
+  assert hasattr(game, 'players')
+  assert hasattr(game, 'bank')
+  assert game.bank.total == 282
+  for i, stack in enumerate(game.bank.coins):
+    assert len(stack) == bankTemplate[i]
+    assert sum(coin.value for coin in stack) == cashTemplate[i]
+  assert hasattr(game, 'deck')
+  assert hasattr(game, 'round')
+  assert hasattr(game, 'notify')
+  assert hasattr(game, 'list_affordable_cards')
+  assert hasattr(game, 'display_cards_to_player')
+  assert hasattr(game, 'take_card_from_stack')
+  assert hasattr(game, 'start')
+  assert hasattr(game, 'play')
+  assert hasattr(game, 'end_game')
+
 class TestGameClass:
   def test_one_player(self):
+    reset_player_names()
     # Arrange
     players = onePlayer
 
@@ -20,6 +42,7 @@ class TestGameClass:
       Game(players)
 
   def test_two_players(self):
+    reset_player_names()
     # Arrange
     players = twoPlayer
 
@@ -27,45 +50,15 @@ class TestGameClass:
     test = Game(players)
 
     # Assert
-    assert hasattr(test, 'name')
-    assert test.name == "Machi Koro"
-
-    assert hasattr(test, 'playerCount')
+    assess_game_class(test)
     assert test.player_count == 2
 
-    assert hasattr(test, 'players')
     for i, player in enumerate(test.players):
       assert player.name == players[i]
       assert player.colour == coloursTemplate[i]
-    
-    assert hasattr(test, 'bank')
-    assert test.bank.total == 282
-    for i, stack in enumerate(test.bank.coins):
-      assert len(stack) == bankTemplate[i]
-      assert sum(coin.value for coin in stack) == cashTemplate[i]
-    
-    assert hasattr(test, 'deck')
-
-    assert hasattr(test, 'round')
-
-    assert hasattr(test, 'limitRounds')
-
-    assert hasattr(test, 'notify')
-
-    assert hasattr(test, 'listAffordableCards')
-
-    assert hasattr(test, 'displayCardsToPlayer')
-
-    assert hasattr(test, 'takeCardFromStack')
-
-    assert hasattr(test, 'start')
-
-    assert hasattr(test, 'play')
-    
-    assert hasattr(test, 'endGame')
-    test.end_game()
 
   def test_three_players(self):
+    reset_player_names()
     # Arrange
     players = threePlayer
 
@@ -73,45 +66,15 @@ class TestGameClass:
     test = Game(players)
 
     # Assert
-    assert hasattr(test, 'name')
-    assert test.name == "Machi Koro"
-
-    assert hasattr(test, 'playerCount')
+    assess_game_class(test)
     assert test.player_count == 3
 
-    assert hasattr(test, 'players')
     for i, player in enumerate(test.players):
       assert player.name == players[i]
       assert player.colour == coloursTemplate[i]
-    
-    assert hasattr(test, 'bank')
-    assert test.bank.total == 282
-    for i, stack in enumerate(test.bank.coins):
-      assert len(stack) == bankTemplate[i]
-      assert sum(coin.value for coin in stack) == cashTemplate[i]
-    
-    assert hasattr(test, 'deck')
-
-    assert hasattr(test, 'round')
-
-    assert hasattr(test, 'limitRounds')
-
-    assert hasattr(test, 'notify')
-
-    assert hasattr(test, 'listAffordableCards')
-
-    assert hasattr(test, 'displayCardsToPlayer')
-
-    assert hasattr(test, 'takeCardFromStack')
-
-    assert hasattr(test, 'start')
-
-    assert hasattr(test, 'play')
-
-    assert hasattr(test, 'endGame')
-    test.end_game()
   
   def test_four_players(self):
+    reset_player_names()
     # Arrange
     players = fourPlayer
 
@@ -119,45 +82,16 @@ class TestGameClass:
     test = Game(players)
 
     # Assert
-    assert hasattr(test, 'name')
-    assert test.name == "Machi Koro"
-
-    assert hasattr(test, 'playerCount')
+    assess_game_class(test)
     assert test.player_count == 4
 
     assert hasattr(test, 'players')
     for i, player in enumerate(test.players):
       assert player.name == players[i]
       assert player.colour == coloursTemplate[i]
-    
-    assert hasattr(test, 'bank')
-    assert test.bank.total == 282
-    for i, stack in enumerate(test.bank.coins):
-      assert len(stack) == bankTemplate[i]
-      assert sum(coin.value for coin in stack) == cashTemplate[i]
-    
-    assert hasattr(test, 'deck')
-
-    assert hasattr(test, 'round')
-
-    assert hasattr(test, 'limitRounds')
-
-    assert hasattr(test, 'notify')
-
-    assert hasattr(test, 'listAffordableCards')
-
-    assert hasattr(test, 'displayCardsToPlayer')
-
-    assert hasattr(test, 'takeCardFromStack')
-
-    assert hasattr(test, 'start')
-
-    assert hasattr(test, 'play')
-
-    assert hasattr(test, 'endGame')
-    test.end_game()
 
   def test_five_players(self):
+    reset_player_names()
     # Arrange
     players = fivePlayer
 
