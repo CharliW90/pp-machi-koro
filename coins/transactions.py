dc5cc131268ec648fa553f6f2d1f17f4e4a6c46b
CharliW90 <acey.williams@googlemail.com>
2024-08-01 22:48:59 +0100

    add ability to silence notifications from transactions

--- coins/transactions.py
+++ coins/transactions.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Union
 if TYPE_CHECKING:
-  from coinage import Coin, CoinPiles
+  from coins import Coin, CoinPiles, Bank
+  from player import Player
 
-def giving(self, total: int) -> list[Coin]:
+def giving(self: Union[Player, Bank], total: int, silent: bool = False) -> list[Coin]:
   given = []
   if total == 0:
     return given
@@ -27,29 +28,28 @@ def giving(self, total: int) -> list[Coin]:
         given.append(self.coins.golds.pop())
       else:
         remaining = 0
-        print(f"{self.name} has no more coins to give.")
 
     total_giving = sum([coin.value for coin in given])
     if remaining > 0:
-      self.declare_action(f"{self.name} is giving {total_giving} coins. {total} requested (payment was short by {remaining}) ==>")
+      if not silent: self.declare_action(f"{self.name} is giving {total_giving} coins. {total} requested (payment was short by {remaining}) ==>")
     elif remaining < 0:
-      self.declare_action(f"{self.name} is giving {total_giving} coins. {total} requested (requires {abs(remaining)} change) ==>")
+      if not silent: self.declare_action(f"{self.name} is giving {total_giving} coins. {total} requested (requires {abs(remaining)} change) ==>")
     else:
-      self.declare_action(f"{self.name} is giving {total_giving} coins ==>")
+      if not silent: self.declare_action(f"{self.name} is giving {total_giving} coins ==>")
     return given
 
-def receiving(self, coins: list[Coin]) -> int:
+def receiving(self: Union[Player, Bank], coins: list[Coin], silent: bool = False) -> int:
   received = 0
   for coin in coins:
     received += coin.value
-    if coin.colour == "Copper":
-      self.coins.coppers.append(coin)
-    elif coin.colour ==  "Silver":
-      self.coins.silvers.append(coin)
-    elif coin.colour == "Gold":
-      self.coins.golds.append(coin)
+    if coin == 1:
+      self.coins.coppers.append(coin)   # type: ignore
+    elif coin == 5:
+      self.coins.silvers.append(coin)   # type: ignore
+    elif coin == 10:
+      self.coins.golds.append(coin)     # type: ignore
   if received > 0:
-    self.declare_action(f"==> {self.name} received {sum([coin.value for coin in coins])} coins")
+    if not silent: self.declare_action(f"==> {self.name} received {sum([coin.value for coin in coins])} coins")
   return received
 
 def calculate_payment(coins: CoinPiles, total: int) -> int:
