53b42f5d8c8f6b21a13451b409c5fccaa606eaef
CharliW90 <acey.williams@googlemail.com>
2024-08-28 20:16:13 +0100

    add time delay and some blank print statements to slow down and space out CLI output, and switch from print() to game.notify() for announcing steps taken

--- setup.py
+++ setup.py
@@ -2,16 +2,21 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
   from player import Player
+  from game import Game
 
 import time
-from actions.dice import roll
+from actions.dice import roll, dice_face
 
-def determine_player_one(players: list[Player]) -> Player:
+def determine_player_one(players: list[Player], game: Game) -> Player:
   player_dict: dict[str, Player] = {player.name: player for player in players}
   player_rolls: list[dict] = []
   for player in players:
     first_roll = roll()
+    player.declare_action(dice_face(first_roll))
+    print() # Insert blank line to console
+    time.sleep(0.2)
     player.declare_action(f"{player.name} rolled {first_roll}")
+    print() # Insert blank line to console
     player_rolls.append({"name": player.name, "roll": first_roll})
     time.sleep(0.5)
 
@@ -19,7 +24,7 @@ def determine_player_one(players: list[Player]) -> Player:
   highest_roll = sorted_players[0]['roll']
   if sorted_players[0]['roll'] > sorted_players[1]['roll']:
     player_one = sorted_players[0]['name']
-    print(f"{player_one} rolled a {highest_roll} and will go first!")
+    game.notify(f"{player_one} rolled a {highest_roll} and will go first!")
     time.sleep(0.5)
     return player_dict[player_one]
   else:
@@ -32,7 +37,7 @@ def determine_player_one(players: list[Player]) -> Player:
     names = [player.name for player in roll_again]
     print(f"Players {' and '.join(names)} rolled a {highest_roll} and need to roll again!")
     time.sleep(0.5)
-    return determine_player_one(roll_again)
+    return determine_player_one(roll_again, game)
   
 def set_turn_orders(all_players: list[Player], player_one: Player) -> None:
   if player_one not in all_players:
